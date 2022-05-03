7d3b18d823661752c9a7f411a673a554cc05fadd
Patrick Dack <patrickdack89@gmail.com>
2022-05-03 17:45:44 -0300

    feat: implementa o vue router

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <router-link to="/">
+      <img src="" alt="logo" id="logo" />
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/orders">Orders</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
