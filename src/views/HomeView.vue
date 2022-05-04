a8bb80ac1b094122b9e0c5afe7401a59835c57e9
Patrick Dack <patrickdack89@gmail.com>
2022-05-04 18:36:16 -0300

    feat/style: cria e estiliza o formulário de cadastro

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,15 +2,18 @@
   <Banner />
   <div class="main-container">
     <h1>Home</h1>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
 import Banner from "../components/Banner.vue";
+import BurgerForm from "../components/BurgerForm.vue";
 
 export default {
   components: {
     Banner,
+    BurgerForm,
   },
 };
 </script>
